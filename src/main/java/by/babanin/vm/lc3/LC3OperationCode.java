7294ee2808870d1797ad1b6e6dd84f94a5db9e0b
Dmitry Babanin <babanin.dima@gmail.com>
2022-05-19 23:13:20 +0400

    Renamed operation codes

--- src/main/java/by/babanin/vm/lc3/LC3OperationCode.java
+++ src/main/java/by/babanin/vm/lc3/LC3OperationCode.java
@@ -7,22 +7,22 @@ import by.babanin.vm.ByteOperationCode;
 import by.babanin.vm.exception.VirtualMachineException;
 
 public enum LC3OperationCode implements ByteOperationCode {
-    OP_BR((byte) 0x00, LC3VirtualMachine::br),     /* branch */
-    OP_ADD((byte) 0x01, LC3VirtualMachine::add),    /* add  */
-    OP_LD((byte) 0x02, LC3VirtualMachine::ld),     /* load */
-    OP_ST((byte) 0x03, LC3VirtualMachine::st),     /* store */
-    OP_JSR((byte) 0x04, LC3VirtualMachine::jsr),    /* jump register */
-    OP_AND((byte) 0x05, LC3VirtualMachine::and),    /* bitwise and */
-    OP_LDR((byte) 0x06, LC3VirtualMachine::ldr),    /* load register */
-    OP_STR((byte) 0x07, LC3VirtualMachine::str),    /* store register */
-    OP_RTI((byte) 0x08, LC3VirtualMachine::rti),    /* unused */
-    OP_NOT((byte) 0x09, LC3VirtualMachine::not),    /* bitwise not */
-    OP_LDI((byte) 0x0A, LC3VirtualMachine::ldi),    /* load indirect */
-    OP_STI((byte) 0x0B, LC3VirtualMachine::sti),    /* store indirect */
-    OP_JMP((byte) 0x0C, LC3VirtualMachine::jmp),    /* jump */
-    OP_RES((byte) 0x0D, LC3VirtualMachine::res),    /* reserved (unused) */
-    OP_LEA((byte) 0x0E, LC3VirtualMachine::lea),    /* load effective address */
-    OP_TRAP((byte) 0x0F, LC3VirtualMachine::trap),   /* execute trap */;
+    BR((byte) 0x00, LC3VirtualMachine::br),     /* branch */
+    ADD((byte) 0x01, LC3VirtualMachine::add),    /* add  */
+    LD((byte) 0x02, LC3VirtualMachine::ld),     /* load */
+    ST((byte) 0x03, LC3VirtualMachine::st),     /* store */
+    JSR((byte) 0x04, LC3VirtualMachine::jsr),    /* jump register */
+    AND((byte) 0x05, LC3VirtualMachine::and),    /* bitwise and */
+    LDR((byte) 0x06, LC3VirtualMachine::ldr),    /* load register */
+    STR((byte) 0x07, LC3VirtualMachine::str),    /* store register */
+    RTI((byte) 0x08, LC3VirtualMachine::rti),    /* unused */
+    NOT((byte) 0x09, LC3VirtualMachine::not),    /* bitwise not */
+    LDI((byte) 0x0A, LC3VirtualMachine::ldi),    /* load indirect */
+    STI((byte) 0x0B, LC3VirtualMachine::sti),    /* store indirect */
+    JMP((byte) 0x0C, LC3VirtualMachine::jmp),    /* jump */
+    RES((byte) 0x0D, LC3VirtualMachine::res),    /* reserved (unused) */
+    LEA((byte) 0x0E, LC3VirtualMachine::lea),    /* load effective address */
+    TRAP((byte) 0x0F, LC3VirtualMachine::trap),   /* execute trap */;
 
     private final byte operationCode;
     private final BiConsumer<LC3VirtualMachine, Short> executor;
@@ -38,7 +38,7 @@ public enum LC3OperationCode implements ByteOperationCode {
     }
 
     public static LC3OperationCode valueOf(short instruction, byte instructionSize) {
-        byte opCode = (byte) (instruction >> instructionSize - 4);
+        byte opCode = (byte) ((instruction >>> instructionSize - 4) & 0x0F);
         return Arrays.stream(LC3OperationCode.values())
                 .filter(lc3OperationCode -> lc3OperationCode.getCode() == opCode)
                 .findFirst()
